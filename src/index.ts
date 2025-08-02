fca3b0dde98f92214eb43d0a6791ce188ec3f4ff
Vitório Corrêa <97925377+VitorioCp@users.noreply.github.com>
2025-08-02 13:09:32 -0300

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://marketin-site-frontend.gtrphk.easypanel.host/", "https://marketin-site-frontend.gtrphk.easypanel.host"],
+    origin: ["http://localhost:3000","https://marketin-site-frontend.gtrphk.easypanel.host/", "https://marketin-site-frontend.gtrphk.easypanel.host", "https://marketin-site-frontend.gtrphk.easypanel.host/login", "http://marketin_site_frontend:80/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
